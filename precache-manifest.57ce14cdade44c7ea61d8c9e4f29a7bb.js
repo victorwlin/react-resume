4024db85455ff6454fc2812b3f68c424ac767f75
Victor Lin <linvictorw@gmail.com>
2021-02-19 20:17:38 +0800

    Updates

---
+++ precache-manifest.57ce14cdade44c7ea61d8c9e4f29a7bb.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "29f8406d1930e5a8694b7b50879c4cd0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47601c2428b421bdfd34",
+    "url": "/static/js/2.e3b0a12e.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.e3b0a12e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3d1d6a7d87fd90b00217",
+    "url": "/static/js/main.5265b13b.chunk.js"
+  },
+  {
+    "revision": "a0d69f97458787781d45",
+    "url": "/static/js/runtime-main.ab52fb89.js"
+  },
+  {
+    "revision": "447ec2368f3c99886f522be9363d5e96",
+    "url": "/static/media/css.447ec236.svg"
+  },
+  {
+    "revision": "be43851e4a59d2952bdff6b966d9a756",
+    "url": "/static/media/firebase.be43851e.png"
+  },
+  {
+    "revision": "14c0d1172557f65f04057f342fa114e7",
+    "url": "/static/media/git.14c0d117.svg"
+  },
+  {
+    "revision": "37b013e202ba4c6393397398b221fa5d",
+    "url": "/static/media/html.37b013e2.svg"
+  },
+  {
+    "revision": "61ed8b054178d0e8d1285ad8f29f4045",
+    "url": "/static/media/javascript.61ed8b05.svg"
+  },
+  {
+    "revision": "71c5bf47b89763352c913a2334cd8172",
+    "url": "/static/media/materialUI.71c5bf47.svg"
+  },
+  {
+    "revision": "f1f5593f9da2df861040dbf9e777adc6",
+    "url": "/static/media/redux.f1f5593f.svg"
+  },
+  {
+    "revision": "88388fc7dc855ba26e2d0ecdf4f07dc7",
+    "url": "/static/media/semanticUI.88388fc7.svg"
+  }
+]);
